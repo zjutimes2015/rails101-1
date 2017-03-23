f787132152bb51f7b4fe65031f4ce76359344f04
zjutimes <zjutimes@gmail.com>
2017-03-23 10:54:11 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new]
   def index
     @groups = Group.all
   end
@@ -43,7 +44,7 @@ class GroupsController < ApplicationController
        redirect_to groups_path
      end
  private
- 
+
  def group_params
    params.require(:group).permit(:title, :description)
  end
