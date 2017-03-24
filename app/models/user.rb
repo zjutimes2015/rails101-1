00512e6a028a71d775b23710a601e059a92fa823
zjutimes <zjutimes@gmail.com>
2017-03-24 11:39:06 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :groups
-  has_many :posts
-end
+         has_many :groups
+         has_many :group_relationships
+         has_many :participated_groups, :through => :group_relationships, :source => :group
+       end
